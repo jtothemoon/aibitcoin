d705be2058d0896c969fde626dc49c44594c4baa
임현진 <jtothemoon@naver.com>
2024-12-01 06:26:41 +0900

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -551,7 +551,9 @@ if __name__ == "__main__":
 
     ## 매일 특정 시간(예: 오전 9시, 오후 3시, 오후 9시)에 실행
     schedule.every().day.at("09:00").do(job)
+    schedule.every().day.at("12:00").do(job)
     schedule.every().day.at("15:00").do(job)
+    schedule.every().day.at("18:00").do(job)
     schedule.every().day.at("21:00").do(job)
     while True:
         schedule.run_pending()
